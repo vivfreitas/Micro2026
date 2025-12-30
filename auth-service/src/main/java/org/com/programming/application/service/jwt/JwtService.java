a0c0c487812268f67ebc720d4a08e7bb4f141a11
Vivian <vivianprofissi7800@gmail.com>
2025-12-30 20:09:32 -0300

    micro

--- auth-service/src/main/java/org/com/programming/application/service/jwt/JwtService.java
+++ auth-service/src/main/java/org/com/programming/application/service/jwt/JwtService.java
@@ -2,11 +2,17 @@ package org.com.programming.application.service.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Service
@@ -22,8 +28,30 @@ public class JwtService {
         return claimsTFunction.apply(claims);
     }
     // Fazer a geração do token (gerarToken)
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+    public String generateToken(Map<String, Objects> extractClaim, UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(extractClaim)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date((System.currentTimeMillis() + 1000 * 60 * 24)))
+                .signWith(sign(), SignatureAlgorithm.HS256)
+                .compact();
+    }
     // Fazer a verificação do token (seTokenEvalido - seTokenEvalido)
+    public boolean ifTokenValid(String token, UserDetails userDetails){
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername()) && !ifTokenExpiration(token));
+    }
+    public boolean ifTokenExpiration(String token){
+        return extractExpiration(token).before(new Date());
+    }
     // Verificar a expiraão do token (extrairExpiracao)
+    public Date extractExpiration(String token){
+        return extractInformationClaim(token, Claims::getExpiration);
+    }
 
     private Claims extractAllClaims(String token){
         return Jwts.parserBuilder()
